9e063df0ced1751589584e8a7701d643f31c68ae
DanielGomez1022 <danielrgomez1996@gmail.com>
2018-10-02 11:27:55 -0700

    add gods

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Elephant from './Elephant'
+import Elephants from './Elephants';
+import GodsContainer from './GodsContainer';
+import GodsContainerLocalstorage from './GodsContainerLocalstorage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Elephant / >
-       
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        <Elephants count={7} />
+        <GodsContainer />
+        <GodsContainerLocalstorage />
+        </header>
       </div>
     );
   }
